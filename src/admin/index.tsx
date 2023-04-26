36718a39134fc1f21a4b1954ad1ff8c13a509b02
Ronak <ronak.malik@lipitech.in>
2023-04-26 22:48:06 +0530

    update bugs

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -45,6 +45,7 @@ const QuestionList = ({
     const [params, setParams] = useState<any>({
         page: 1,
         perPage: 10,
+        recent: true
     });
     const [questions, setQuestions] = React.useState<any>({
         totalItems: 0,
